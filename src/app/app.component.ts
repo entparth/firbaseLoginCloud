9b6e9714ac38882cc16c7107c958cd7bb107cc3b
entparth <parth.sharma@eytech.com>
2018-06-05 19:25:47 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,61 @@
+import { Component, ViewChild } from '@angular/core';
+import { Nav, Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
+  rootPage: any = HomePage;
+
+  pages: Array<{title: string, component: any}>;
+
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+    this.initializeApp();
+
+    // used for an example of ngFor and navigation
+    this.pages = [
+      { title: 'Home', component: HomePage },
+      { title: 'List', component: ListPage }
+    ];
+
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
+  }
+
+  initializefirebase(){
+
+<script src="https://www.gstatic.com/firebasejs/5.0.4/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBQZUm4yeHl131xEHFXt-mOYrnIPHS-sE4",
+    authDomain: "adrdatabase-55a01.firebaseapp.com",
+    databaseURL: "https://adrdatabase-55a01.firebaseio.com",
+    projectId: "adrdatabase-55a01",
+    storageBucket: "adrdatabase-55a01.appspot.com",
+    messagingSenderId: "396843166583"
+  };
+  firebase.initializeApp(config);
+</script>
+  }
+}
